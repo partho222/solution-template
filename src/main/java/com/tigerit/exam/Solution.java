9032216a8a96c1d6645914755723e5fa53e84601
Tariq <partho222@gmail.com>
2017-08-07 23:31:36 +0600

    table class added

--- src/main/java/com/tigerit/exam/Solution.java
+++ src/main/java/com/tigerit/exam/Solution.java
@@ -1,6 +1,5 @@
 package com.tigerit.exam;
 
-
 import static com.tigerit.exam.IO.*;
 
 /**
@@ -10,6 +9,43 @@ import static com.tigerit.exam.IO.*;
  * application's execution points start from inside run method.
  */
 public class Solution implements Runnable {
+/***************************************************************************************/
+    /*
+    * Created inner class 'Table' as
+    * I have to implement application
+    * logic inside solution class
+    * */
+
+    public class Table {
+        private String name;
+        private String alias;
+        private String[] columnName;
+        private Integer noOfColumn;
+        private Integer noOfRow;
+        private Integer[][] data;
+
+        public Table(String name, Integer noOfColumn, Integer noOfRow) {
+            this.name = name;
+            this.noOfColumn = noOfColumn;
+            this.noOfRow = noOfRow;
+            this.columnName = new String[noOfColumn];
+            this.data = new Integer[noOfColumn][noOfRow];
+        }
+
+        public void insertColumnName(String... names) {
+            for(Integer i = 0; i < names.length; i++) {
+                this.columnName[i] = names[i];
+            }
+        }
+
+        public void insertValue(Integer row, Integer... values) {
+            for(Integer i = 0; i < values.length; i++) {
+                this.data[row][i] = values[i];
+            }
+        }
+    }
+/***************************************************************************************/
+
     @Override
     public void run() {
         // your application entry point
