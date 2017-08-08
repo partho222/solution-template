abe18bee160df8cb2caba519a05fa061d118822c
Tariq <partho222@gmail.com>
2017-08-08 21:25:13 +0600

    implementation (WIP), table creation done

--- src/main/java/com/tigerit/exam/Solution.java
+++ src/main/java/com/tigerit/exam/Solution.java
@@ -1,6 +1,7 @@
 package com.tigerit.exam;
 
 import static com.tigerit.exam.IO.*;
+import static com.tigerit.exam.Utils.*;
 
 /**
  * All of your application logic should be placed inside this class.
@@ -9,46 +10,21 @@ import static com.tigerit.exam.IO.*;
  * application's execution points start from inside run method.
  */
 public class Solution implements Runnable {
-/***************************************************************************************/
-    /*
-    * Created inner class 'Table' as
-    * I have to implement application
-    * logic inside solution class
-    * */
 
-    public class Table {
-        private String name;
-        private String alias;
-        private String[] columnName;
-        private Integer noOfColumn;
-        private Integer noOfRow;
-        private Integer[][] data;
+    private Table[] tables;
 
-        public Table(String name, Integer noOfColumn, Integer noOfRow) {
-            this.name = name;
-            this.noOfColumn = noOfColumn;
-            this.noOfRow = noOfRow;
-            this.columnName = new String[noOfColumn];
-            this.data = new Integer[noOfColumn][noOfRow];
-        }
-
-        public void insertColumnName(String... names) {
-            for(Integer i = 0; i < names.length; i++) {
-                this.columnName[i] = names[i];
-            }
-        }
-
-        public void insertValue(Integer row, Integer... values) {
-            for(Integer i = 0; i < values.length; i++) {
-                this.data[row][i] = values[i];
-            }
-        }
-    }
-/***************************************************************************************/
     public void input() {
         Integer kase = readLineAsInteger();
         while (kase > 0) {
             Integer noOfTable = readLineAsInteger();
+            this.tables = new Table[noOfTable];
+            Integer itr = 0;
+            while (noOfTable > 0) {
+                String tableName = readLine();
+                Integer[] values = getIntFromLine(readLine());
+                this.tables[itr++] = new Table(tableName, values[0], values[1]);
+                /* data insertion operation */
+            }
         }
     }
 
